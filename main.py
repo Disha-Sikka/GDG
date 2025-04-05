522b6770f099f2b64ee7463c060c5a540bfb9d81
Disha-Sikka <disha.code77@gmail.com>
2025-04-05 14:15:42 +0530

    Update main.py
    nothing in this file

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-from capture_face import capture_voter_image
-from verify_voter import verify_voter
-
-# Step 1: Enter voter name
-voter_name = input("Enter Voter's Name: ")
-voter_age = input("Enter voter age: ")
-
-# Step 2: Capture the voter's face
-capture_voter_image(voter_name,voter_age)
-
-# Step 3: Verify if the voter is registered
-if verify_voter(voter_name):
-    print("Voter verified! Proceed to vote.")
-else:
-    print("Voter NOT registered!")
 
